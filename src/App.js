4dae3ef15c10211321d88713136e11e03225ba37
JacobIm28 <84198748+JacobIm28@users.noreply.github.com>
2021-12-20 16:08:30 -0500

    Update

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
 import "./App.css";
-import { StartPage } from "./components/StartPage";
-import { StartButton } from "./components/StartButton";
 import { Board } from "./components/Board";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 //import {Link} from 'react-router-dom';
 
 function App() {
   return (
     <div>
-      <header className="title">TicTacToe</header>
-      <h1 className="name">Jacob Im</h1>
+      <header>
+        <h1 className="title">TicTacToe</h1>
+        <h2 className="name">Jacob Im</h2>
+      </header>
       <Board />
     </div>
   );
